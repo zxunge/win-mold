55678bbde29097e6b1ead769e4d9419ab2c57e0a
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-19 18:53:56 +0800

    Do not install dependencies.

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ NAME=win-mold-${BUILDNO}
 
 git clone --branch stable https://github.com/rui314/mold.git
 cd mold
-./install-build-deps.sh
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${BINARY} -DCMAKE_CXX_COMPILER=c++ -B build
 cmake --build build -j$(nproc)
 cmake --build build --target install
